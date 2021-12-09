9da3378fc72c1bf6b3b6a1a7dca214fd37e94a12
Mubeen, Shuweb <Shuweb_Mubeen@intuit.com>
2021-12-09 11:21:00 -0700

    add voter

--- soaringeagle-app/src/components/registration/VoterRegistrationForm.js
+++ soaringeagle-app/src/components/registration/VoterRegistrationForm.js
@@ -1,4 +1,5 @@
-import { useForm } from '../hooks/useForm';
+import { useForm } from "../../hooks/useForm";
+
 
 export const VoterRegistrationForm = props => {
 
@@ -14,7 +15,7 @@ export const VoterRegistrationForm = props => {
   });
 
   const submitCar = () => {
-    props.onSubmitCar({ ...voterRegistrationForm });
+    props.onSubmitVoterRegistration({ ...voterRegistrationForm });
 
     resetVoterRegistrationForm();
   };
@@ -22,36 +23,32 @@ export const VoterRegistrationForm = props => {
   return (
     <form>
       <label>
-        Make:
-        <input type="text" name="id" value={voterRegistrationForm.id} onChange={change} />
-      </label>
-      <label>
-        Model:
+        First Name:
         <input type="text" name="firstName" value={voterRegistrationForm.firstName} onChange={change} />
       </label>
       <label>
-        Year:
-        <input type="number" name="lastName" value={voterRegistrationForm.lastName} onChange={change} />
+        Last Name:
+        <input type="text" name="lastName" value={voterRegistrationForm.lastName} onChange={change} />
       </label>
       <label>
-        Color:
+        Address:
         <input type="text" name="address" value={voterRegistrationForm.address} onChange={change} />
       </label>
       <label>
-        Price:
-        <input type="number" name="city" value={voterRegistrationForm.city} onChange={change} />
+        City:
+        <input type="text" name="city" value={voterRegistrationForm.city} onChange={change} />
       </label>
     <label>
-    Price:
+    Birth Date:
     <input type="number" name="birthDate" value={voterRegistrationForm.birthDate} onChange={change} />
     </label>
     <label>
-    Price:
-    <input type="number" name="email" value={voterRegistrationForm.email} onChange={change} />
+    Email:
+    <input type="text" name="email" value={voterRegistrationForm.email} onChange={change} />
     </label>
     <label>
-    Price:
-    <input type="number" name="phone" value={voterRegistrationForm.phone} onChange={change} />
+    Phone:
+    <input type="text" name="phone" value={voterRegistrationForm.phone} onChange={change} />
     </label>
       <button type="button" onClick={submitCar}>{props.buttonText}</button>
     </form>
--- soaringeagle-app/src/components/registration/VoterTool.js
+++ soaringeagle-app/src/components/registration/VoterTool.js
@@ -1,12 +1,13 @@
 
 import { VoterTableContainer } from "./VoterTableContainer";
+import { VoterRegistrationFormContainer } from "./VoterRegistrationFormContainer";
 
 export const RegistrationTool = () => {
 
   return (
     <>
       <VoterTableContainer />
-      {/*<CarFormContainer />*/}
+      <VoterRegistrationFormContainer />
     </>
   );
 
